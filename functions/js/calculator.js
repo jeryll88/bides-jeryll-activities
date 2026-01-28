1e0df49d958e88f72f2032dc83df994814245235
jeryll88 <jeryll88>
2026-01-28 10:56:04 +0800

    basic calculator

--- functions/js/calculator.js
+++ functions/js/calculator.js
@@ -1,13 +1,50 @@
 const display = document.getElementById("display");
+const display1 = document.getElementById("display1");
+
+let firstnum = "";
+let operator = "";
 
 function appendValue(value) {
-    display.value += value;
+    if (display.value === "0") {
+        display.value = value;
+    } else {
+        display.value += value;
+    }
 }
+
+function add(value) {
+    firstnum = display.value;
+    operator = value;
+
+    display1.value = operator;
+    display.value = "0";
+}
+
+function calculate() {
+  const secondnum = display.value;
+  let result;
+
+  if (operator === "+") {
+    result = Number(firstnum) + Number(secondnum);
+  } else if (operator === "-") {
+    result = Number(firstnum) - Number(secondnum);
+  } else if (operator === "*") {
+    result = Number(firstnum) * Number(secondnum);
+  } else if (operator === "/") {
+    result = Number(firstnum) / Number(secondnum);
+  }
+
+  display.value = result;
+  display1.value = "";
+}
+
 
 function clearDisplay() {
-    display.value = "";
-}
-function clearOne() {
-    display.value = display.value.slice(0, -1);
+    display.value = "0";
+    display1.value = "";
 }
 
+function clearOne() {
+    display.value = display.value.slice(0, -1);
+    if (display.value === "") display.value = "0";
+}
